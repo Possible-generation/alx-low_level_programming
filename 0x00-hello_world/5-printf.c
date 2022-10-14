2fc99f2a3bafc9910230b26da95db02cd5a18cd5
Possible-generation <ajigbotoshojoshua2000@gmail.com>
2022-10-14 08:06:17 -0700

    prnt with printf on c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Entry piont
+ *
+ * Return: Always 0 (Succes)
+ */
+int main(void)
+{
+	printf("with the proper grammer, but the outcomeis a piece of art,\n");
+	return (0);
+}
