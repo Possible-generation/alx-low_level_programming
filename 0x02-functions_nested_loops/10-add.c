0b99e1e3021a3b0c60e5f9ed4859b470c659ab2d
Possible-generation <ajigbotoshojoshua2000@gmail.com>
2022-10-19 10:39:45 -0700

    10 task

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * add - Adds twointegers and rerurns
+ * the resilt
+ *
+ * @a: number one.
+ *
+ * @b: number two.
+ *
+ * Return: Add of number one and number two.
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
