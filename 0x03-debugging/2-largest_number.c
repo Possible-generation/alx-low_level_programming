d115499fe5c35dced8a9049fa26eb6c3fe4791b8
Possible-generation <ajigbotoshojoshua2000@gmail.com>
2022-10-20 06:20:15 -0700

    2 task

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -17,11 +17,11 @@ int largest_number(int a, int b, int c)
 	{
 		largest = a;
 	}
-	if (b > a && a > c)
+	else if (b > a && a > c)
 	{
 		largest = b;
 	}
-	if (c > a && c > b)
+	else
 	{
 		largest = c;
 	}
