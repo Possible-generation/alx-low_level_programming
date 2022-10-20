19f7f64d7ab725060c1208274a9bc15544cf2659
Possible-generation <ajigbotoshojoshua2000@gmail.com>
2022-10-19 19:11:06 -0700

    new change

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -8,7 +8,7 @@
  * Return: no return
  */
 
- void print_times_table(int n)
+void print_times_table(int n)
 {
 	int a, b, op;
 
@@ -17,7 +17,6 @@
 		for (a = 0; a <= n; a++)
 		{
 			_putchar(48);
-			
 			for (b = 1; b <= n; b++)
 			{
 				op = a * b;
@@ -28,19 +27,19 @@
 				{
 					_putchar(32);
 					_putchar(32);
-					_putchar(op +48);
+					_putchar(op + 48);
 				}
 				else if (op <= 99)
 				{
 					_putchar(32);
 					_putchar((op / 10) + 48);
-					_putchar((op % 10)+48);
+					_putchar((op % 10) + 48);
 				}
 				else
 				{
 					_putchar(((op / 100) % 10) + 48);
-					_putchar(((op / 10) % 10) +48);
-					_putchar((op % 10) +48);
+					_putchar(((op / 10) % 10) + 48);
+					_putchar((op % 10) + 48);
 				}
 			}
 			_putchar('\n');
