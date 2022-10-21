b93e922909052dc3ce424effc26f89e5017bfb9c
Possible-generation <ajigbotoshojoshua2000@gmail.com>
2022-10-20 20:47:25 -0700

    new work

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+
+/**
+ * _isupper-check if character is lowercase
+ * @c: character to check if it is lowercase
+ * Return: 0-if lowecase,1-if not
+ */
+
+int _isupper(int c)
+{
+	if ((c >= 'a') && (c <= 'z'))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
