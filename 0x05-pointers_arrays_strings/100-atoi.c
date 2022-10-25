39add811711d975aa98cb3be740b5cf234f336de
Possible-generation <ajigbotoshojoshua2000@gmail.com>
2022-10-25 02:44:43 -0700

    task 100

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,6 +1,4 @@
 #include "main.h"
-#include <stdio.h>
-
 
 /**
  * _atoi - convert a string to an integer
@@ -27,7 +25,7 @@ int _atoi(char *s)
 		while (s[c] >= 48 && s[c] <= 57)
 		{
 			isi = 1;
-			ni = (ni * 10) + (s[c] - '0')
+			ni = (ni * 10) + (s[c] - '0');
 			c++;
 		}
 
