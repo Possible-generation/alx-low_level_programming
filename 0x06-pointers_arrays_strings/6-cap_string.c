78955978b2913e900d010adbd991afb4957cc1c6
Possible-generation <ajigbotoshojoshua2000@gmail.com>
2022-10-27 10:59:18 -0700

    task 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -14,7 +14,7 @@ char *cap_string(char *s)
 
 	while (*(s + i))
 	{
-		if (*(s + 1) >= 'a' && *(s + 1) <= 'z')
+		if (*(s + i) >= 'a' && *(s + i) <= 'z')
 		{
 			if (i == 0)
 				*(s + i) -= 'a' - 'A';
@@ -23,7 +23,7 @@ char *cap_string(char *s)
 				for (j = 0; j <= 12; j++)
 				{
 					if (a[j] == *(s + i - 1))
-						*(s + 1) -= 'a' - 'A';
+						*(s + i) -= 'a' - 'A';
 				}
 			}
 		}
